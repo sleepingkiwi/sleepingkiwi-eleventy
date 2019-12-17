9fa27905f565e48cb3c082f9a0f505d51ce4df4c
sleepingkiwi <joe@sleeping.kiwi>
2019-12-17 18:03:26 +0000

    Replace sass with postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,30 @@
-module.exports = ({
+// ref: https://github.com/postcss/postcss-cli#context
+// because we use postcss from the commmand line we can use this function to get `ctx` context,
+// allows us to configure settings based on the file/settings used on the CL
+
+module.exports = (ctx) => ({
+  map: ctx.options.map,
+  // parser: ctx.options.parser,
   plugins: {
-    autoprefixer: true,
-    cssnano: {
-      preset: 'default',
+    'postcss-import': { root: ctx.file.dirname },
+    'postcss-preset-env': {
+      stage: 3,
+      features: {
+        'custom-media-queries': { preserve: false },
+        'nesting-rules': true,
+        'custom-properties': { preserve: true },
+      },
+      // autoprefixer is included in postcssPresetEnv:
+      // https://github.com/csstools/postcss-preset-env
+      autoprefixer: {
+        // setting remove to false means old prefixes aren't stripped if we manually include them
+        // sometimes we might want to manually add a prefix for a hack or specific targetting...
+        remove: false,
+      },
     },
+    cssnano: ctx.env === 'production'
+      ? {
+        preset: 'default',
+      } : false,
   },
 });
---
+++ src/styles/readme.md
@@ -0,0 +1,5 @@
+# style structure
+
+we run `postcss-import` against all `.css` files in the root of this dir.
+
+Files that are not imported by one of the root files are not added to the build.
