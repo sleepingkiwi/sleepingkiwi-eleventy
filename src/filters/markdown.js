837d330355a1a685a24f7584b98e34172daafc5b
sleepingkiwi <joe@sleeping.kiwi>
2021-01-11 20:54:01 +0000

    Add noopener to external and ALL markdown links

--- src/filters/markdown.js
+++ src/filters/markdown.js
@@ -6,4 +6,16 @@ const markdownIt = require('markdown-it')({
   linkify: true,
 });
 
+// rel noopener on all links
+// TODO - check own URL and only apply to external?
+// https://github.com/markdown-it/markdown-it/issues/46
+const fallbackFunc = (tokens, idx, options, env, self) => self.renderToken(tokens, idx, options);
+const oldRender = markdownIt.renderer.rules.link_open || fallbackFunc;
+
+markdownIt.renderer.rules.link_open = (tokens, idx, options, env, self) => {
+  tokens[idx].attrPush(['rel', 'noopener']);
+  return oldRender(tokens, idx, options, env, self);
+};
+
+
 module.exports = (value) => markdownIt.render(value);
