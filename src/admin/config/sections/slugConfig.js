29c361282f44763b99d7967e795e8c6420b3ed0d
sleepingkiwi <joe@sleeping.kiwi>
2021-10-02 18:29:11 +0100

    Add a warning to slug field about breaking existing links to old URLs

--- src/admin/config/sections/slugConfig.js
+++ src/admin/config/sections/slugConfig.js
@@ -10,17 +10,18 @@
 export const slugConfig = [
   // allow slug changes
   {
-    label: 'Slug',
+    label: 'Slug/URL',
     name: 'slugInstructions',
     widget: 'instructions',
-    flavour: 'vanilla',
-    instructions: 'The slug is used in this page\'s URL \nIt defaults to the title of the page but you can change it to whatever you want. \n`admin` and `index` cannot be used as slugs. \nThis field only allows alphanumeric characters and hyphens.',
+    flavour: 'header',
+    instructions: 'The slug is used as this page\'s URL \nIt defaults to the title of the page but you can change it to whatever you want.\n- \n`admin` and `index` cannot be used as slugs. \nThis field only allows alphanumeric characters and hyphens.',
     required: false,
   },
   {
     label: 'Slug',
     name: 'slug',
     widget: 'slug',
+    hint: '⚠ Changing this field will change the URL for this page. Any existing links to the old URL will 404.',
     // eslint-disable-next-line no-useless-escape
     pattern: ['^(?!admin$)(?!index$)(?!Admin$)(?!Index$)[A-Za-z-d]+', 'Please only use alphanumeric characters and hyphens. `admin` and `index` are forbidden'],
   },
