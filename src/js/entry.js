04ebf31defdec7c8c9ec6f96401cd50402dd13d8
sleepingkiwi <joe@sleeping.kiwi>
2022-05-19 13:13:19 +0100

    Make sure that JS which uses the dom only runs after DOM is ready!

--- src/js/entry.js
+++ src/js/entry.js
@@ -7,40 +7,46 @@ import { soModern, soFunctional } from './test';
 import CounterOne from './svelte/CounterOne.svelte';
 import CounterTwo from './svelte/CounterTwo.svelte';
 
-
-/** babel transpiles things...
+/** Init everything that interacts with DOM (after dom is ready)
  *  ------------------------------------------------------------------------------------------------
 **/
-console.log(`Oh. ${soModern()}`);
-soFunctional().forEach((l) => console.log(l));
+const init = () => {
+  // validation listeners for forms
+  formsInit();
+  // allowing elements to opt in to generic interesection observation
+  // by adding .js--wants-interestion
+  genericIntersectionInit();
 
+  /** babel transpiles things...
+   *  ----------------------------------------------------------------------------------------------
+  **/
+  console.log(`Oh. ${soModern()}`);
+  soFunctional().forEach((l) => console.log(l));
 
-/** Init everything
- *  ------------------------------------------------------------------------------------------------
-**/
-// validation listeners for forms
-formsInit();
-// allowing elements to opt in to generic interesection observation by adding .js--wants-interestion
-genericIntersectionInit();
+  /** Svelte
+   *  ----------------------------------------------------------------------------------------------
+   *  basic svelte integration
+  **/
+  const sOne = document.querySelector('.js--CounterOne');
+  const sTwo = document.querySelector('.js--CounterTwo');
 
+  if (sOne) {
+    // eslint-disable-next-line no-unused-vars
+    const one = new CounterOne({
+      target: sOne,
+    });
+  }
 
-/** Svelte
- *  ------------------------------------------------------------------------------------------------
- *  basic svelte integration
-**/
-const sOne = document.querySelector('.js--CounterOne');
-const sTwo = document.querySelector('.js--CounterTwo');
+  if (sTwo) {
+    // eslint-disable-next-line no-unused-vars
+    const two = new CounterTwo({
+      target: sTwo,
+    });
+  }
+};
 
-if (sOne) {
-  // eslint-disable-next-line no-unused-vars
-  const one = new CounterOne({
-    target: sOne,
-  });
-}
-
-if (sTwo) {
-  // eslint-disable-next-line no-unused-vars
-  const two = new CounterTwo({
-    target: sTwo,
-  });
+if (document.readyState === 'loading') { // Loading hasn't finished yet
+  document.addEventListener('DOMContentLoaded', init);
+} else { // `DOMContentLoaded` has already fired
+  init();
 }
